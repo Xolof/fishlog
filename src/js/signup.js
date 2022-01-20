fe09764b123b34aba506caf49b38f71bafc31997
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2022-01-20 21:47:13 +0100

    Try to make browser not autocomplete data for signup

--- src/js/signup.js
+++ src/js/signup.js
@@ -8,9 +8,9 @@ export const signup = function() {
         <div class="content_inner">
             <h2>Sign up</h2>
             <form action="" id="add-form">
-                <input type="text" id="name" placeholder="Username" class="input">
-                <input type="text" id="email" placeholder="Email" class="input">
-                <input type="password" id="password" placeholder="Password" class="input">
+                <input type="text" id="input-field-1" placeholder="U&#8204;sername" class="input" autocomplete="off">
+                <input type="text" id="input-field-2" placeholder="Em&#8204;ail" class="input" autocomplete="off">
+                <input type="password" id="input-field-3" placeholder="Pas&#8204;sword" class="input" autocomplete="off">
                 <button id="signup" class="button">Sign up</button>
             </form>
         </div>
@@ -19,11 +19,11 @@ export const signup = function() {
         helpers.addListener("click", helpers.getId("signup"), (e) => {
             e.preventDefault();
 
-            const name = helpers.getId("name").value;
-            const email = helpers.getId("email").value;
-            const password = helpers.getId("password").value;
+            const name = helpers.getId("input-field-1").value;
+            const email = helpers.getId("input-field-2").value;
+            const password = helpers.getId("input-field-3").value;
             
-            if (email && password) {
+            if (name && email && password) {
                 const data = {
                     name,
                     email,
