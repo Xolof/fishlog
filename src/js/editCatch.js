aec71c572b47e1f8c29358a3e250f01f0925a1bb
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2022-01-23 17:09:07 +0100

    Fix bug related to editing

--- src/js/editCatch.js
+++ src/js/editCatch.js
@@ -1,4 +1,5 @@
 import { helpers } from "./helpers.js";
+import { state } from "./state.js";
 import { showMap as showMapView } from "./showMap.js";
 import { addUserPosition } from "./adduserposition.js";
 import { api } from "./api.js";
@@ -9,14 +10,30 @@ export const editCatch = function() {
     let coordinates = false;
     let updatePositionInterval;
 
-    async function getCatch (id) {      
+    async function getCatch (id) {
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer " + localStorage.getItem("token"));
+        
+        var requestOptions = {
+          method: 'GET',
+          headers: myHeaders,
+          redirect: 'follow'
+        };
+        
         try {
-            return await api.getCatch(id);
+            const res = await fetch(`${API_URL}/api/fishcatch/${id}`, requestOptions)
+            const json = await res.json();
+            return json;
         } catch (e) {
             console.error(e);
             helpers.showFlashMessage("The request failed.", "error");
         }
     }
+
+    // function hideMap () {
+    //     let map = helpers.getId("map");
+    //     map.parentNode.removeChild(map);
+    // }
 
     function showMap (location) {
         clearInterval(updatePositionInterval);
@@ -51,22 +68,38 @@ export const editCatch = function() {
     async function init(id) {
         const content = helpers.getId("content");
 
+        if (!state.getLoggedIn()) {
+            content.innerHTML = `
+                <div class="content_inner">
+                    <h2>Add</h2>
+                    <div class="img_wrapper">
+                        <img src="./images/atlantic_mackerel_2.jpg" >
+                    </div>
+                    <div class="info">
+                        <p>You have to log in to be able to add a catch.</p>
+                        <p>Sign in or create an account.</p>
+                    </div>
+                </div>
+                `;
+            return;
+        };
+
         const catchData = await getCatch(id);
 
         content.innerHTML = `
         <div class="content_inner">
             <h2>Edit</h2>
-            <form action="" id="edit-form">
-                <input type="text" id="edit_input_species" placeholder="Species" class="input" value="${catchData.species}">
-                <input type="number" min="0" id="edit_input_length" placeholder="Length (cm)" class="input" value="${catchData.length}">
-                <input type="number" min="0" id="edit_input_weight" placeholder="Weight (g)" class="input" value="${catchData.weight}">
-                <input type="date" id="edit_input_date" class="input" value="${catchData.date}">
+            <form action="" id="add-form">
+                <input type="text" id="add_input_species" placeholder="Species" class="input" value="${catchData.species}">
+                <input type="number" id="add_input_length" placeholder="Length (cm)" class="input" value="${catchData.length}">
+                <input type="number" id="add_input_weight" placeholder="Weight (g)" class="input" value="${catchData.weight}">
+                <input type="date" id="add_input_date" class="input" value="${catchData.date}">
                 <p>Click on the map to set location</p>
                 <div id="map"></div>
                 <img src="${API_URL}${catchData.imageurl}" alt="Catch image" id="preview_image" />
                 <label for="uploadImage" id="uploadImageLabel">Change image</label>
                 <input type="file" id="uploadImage">
-                <button id="edit_catch" class="button">Save</button>
+                <button id="add_catch" class="button">Save</button>
             </form>
         </div>
         `;
@@ -76,6 +109,7 @@ export const editCatch = function() {
             document.getElementById("preview_image").src = src;
         });
 
+
         helpers.addListener("keypress", window, (e) => {
             if (e.target.type === "number") {
                 if (isNaN(e.key)) {
@@ -84,17 +118,17 @@ export const editCatch = function() {
             }
         });
 
-        const editButton = helpers.getId("edit_catch");
+        const addButton = helpers.getId("add_catch");
 
         showMap(catchData.location);
 
-        helpers.addListener("click", editButton, async (e) => {
+        helpers.addListener("click", addButton, (e) => {
             e.preventDefault();
 
-            const species = helpers.getId("edit_input_species").value;
-            const length = helpers.getId("edit_input_length").value;
-            const weight = helpers.getId("edit_input_weight").value;
-            const date = helpers.getId("edit_input_date").value;
+            const species = helpers.getId("add_input_species").value;
+            const length = helpers.getId("add_input_length").value;
+            const weight = helpers.getId("add_input_weight").value;
+            const date = helpers.getId("add_input_date").value;
             const uploadImageEl = helpers.getId("uploadImage");
 
             const data = {
@@ -112,19 +146,38 @@ export const editCatch = function() {
                 data.uploadImage = uploadImageEl.files[0];
             }
 
-            const json = await api.editCatch(data, id);
+            postData (data, id);
+        })
+    }
 
-            if (json.success) {
-                helpers.showFlashMessage("Catch updated!", "success");
-                helpers.resetContent();
-                const splitLocation = data.location.split(",");
-                showMapView.init([splitLocation[0], splitLocation[1]], json.data.id);
-            } else if (json.error) {
-                helpers.showFlashMessage(json.error, "error");
-            } else {
-                helpers.showFlashMessage("The request failed.", "error");
-            }
+    async function postData (data, id) {
+        let formData = new FormData();
+        for (const key in data) {
+            formData.append(key, data[key])
+        }
+
+        const res = await fetch(`${API_URL}/api/update/${id}`, {
+            method: "POST", // Must use post because in PHP PUT/PATCH request with multipart/form-data will not populate $_FILES.
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            },
+            body: formData,
+            redirect: "follow"
         });
+        
+        const json = await res.json();
+
+        if (json.success) {
+            helpers.showFlashMessage("Catch updated!", "success");
+            helpers.resetContent();
+            const splitLocation = data.location.split(",");
+            showMapView.init([splitLocation[0], splitLocation[1]]);
+        } else if (json.error) {
+            console.error(json.error);
+            helpers.showFlashMessage(json.error, "error");
+        } else {
+            helpers.showFlashMessage("The request failed.", "error");
+        }
     }
 
     return {
