4083228d656e9c0e33d780a0c53c9285c2437e44
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2022-01-23 14:20:29 +0100

    Improve flashmessage

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -21,17 +21,29 @@ export const helpers = function() {
 
     function showFlashMessage(message, css) {
         const app = getId("app");
-        const flashMessage = document.createElement("p");
+        const flashMessage = document.createElement("ul");
         flashMessage.setAttribute("id", "flashmessage");
         flashMessage.style.zIndex = flashZ;
         flashZ += 1;
         app.prepend(flashMessage);
-        flashMessage.textContent = message;
+
+        if (message instanceof Array) {
+            message.forEach(item => {
+                const li = document.createElement("li");
+                li.textContent = item;
+                flashMessage.appendChild(li);
+            });
+        } else {
+            const li = document.createElement("li");
+            li.textContent = message;
+            flashMessage.appendChild(li);
+        }
+
         flashMessage.classList.add(css);
         flashMessage.classList.add("slide");
         setTimeout(() => {
             app.removeChild(flashMessage);
-        }, 4000)
+        }, 4000);
     }
 
     return {
